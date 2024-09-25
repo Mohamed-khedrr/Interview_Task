05494b4af95222e78261ef37d8e48e92d3dd9cf2
Khedr <khedr66664@gmail.com>
2024-09-25 15:16:33 +0300

    quesiton service

---
+++ src/app/services/questions-service.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class QuestionsServicesService {
+  visibleList = [
+    { placeholder: 'حقل أسم العميل', iconsUrl: 'imgs/pen-to-square.svg' },
+    { placeholder: 'حقل رقم الهاتف', iconsUrl: 'imgs/call.svg' },
+    { placeholder: 'حقل البريد الألكتروني', iconsUrl: 'imgs/mail-outline.svg' },
+    { placeholder: 'حقل المنتجات', iconsUrl: 'imgs/mail-outline.svg' },
+    { placeholder: 'حقل الملاحظات', iconsUrl: 'imgs/mail-outline.svg' },
+  ];
+
+  hiddenList = [
+    { placeholder: 'حقل مخصص 1', iconsUrl: 'imgs/pen-field.svg' },
+    { placeholder: 'حقل مخصص 2', iconsUrl: 'imgs/pen-field.svg' },
+    { placeholder: 'حقل مخصص 3', iconsUrl: 'imgs/pen-field.svg' },
+  ];
+
+  constructor() {}
+}
